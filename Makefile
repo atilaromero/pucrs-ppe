65332f2360f7f44725a07fef868ea6e4a8fe39b7
Atila Romero <atilaromero@gmail.com>
2018-10-24 11:33:01 -0300

    modified:   Makefile 	new file:   matrix_multi4.cpp 	new file:   primer_cpp1.cpp

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: m1 m2 m3
+all: m1 m2 m3 m4 p1
 m1: matrix_multi1.cpp
 	g++ -std=c++1y -O3 -ltbb -lpthread matrix_multi1.cpp -o m1
 
@@ -7,3 +7,9 @@ m2: matrix_multi2.cpp
 
 m3: matrix_multi3.cpp
 	g++ -std=c++1y -O3 -ltbb -lpthread matrix_multi3.cpp -o m3
+
+m4: matrix_multi4.cpp
+	g++ -std=c++1y -O3 -ltbb -lpthread matrix_multi4.cpp -o m4
+
+p1: primer_cpp1.cpp
+	g++ -std=c++1y -O3 -ltbb -lpthread primer_cpp1.cpp -o p1
---
+++ matrix_multi4.cpp
@@ -0,0 +1,175 @@
+/* ***************************************************************************
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  As a special exception, you may use this file as part of a free software
+ *  library without restriction.  Specifically, if other files instantiate
+ *  templates or use macros or inline functions from this file, or you compile
+ *  this file and link it with other files to produce an executable, this
+ *  file does not by itself cause the resulting executable to be covered by
+ *  the GNU General Public License.  This exception does not however
+ *  invalidate any other reasons why the executable file might be covered by
+ *  the GNU General Public License.
+ *
+ ****************************************************************************
+ *  Authors: Dalvan Griebler <dalvangriebler@gmail.com>
+ *         
+ *  Copyright: GNU General Public License
+ *  Description: This is a simple matrix multiplication algorithm. 
+ *  File Name: matrix_multi.cpp
+ *  Version: 1.0 (25/05/2018)
+ *  Compilation Command: g++ -std=c++1y matrix_multi.cpp -o exe
+ ****************************************************************************
+*/
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+
+namespace comm {
+
+	template <typename T>
+	class queue
+	{
+	private:
+		std::mutex              d_mutex;
+		std::condition_variable d_condition;
+		std::deque<T>         d_queue;
+	public:
+		void push(T const& value) {
+			{
+				std::unique_lock<std::mutex> lock(this->d_mutex);
+				d_queue.push_front(value);
+			}
+			this->d_condition.notify_one();
+		}
+		T pop() {
+			std::unique_lock<std::mutex> lock(this->d_mutex);
+			this->d_condition.wait(lock, [=]{ return !this->d_queue.empty(); });
+			T rc(std::move(this->d_queue.back()));
+			this->d_queue.pop_back();
+			return rc;
+		}
+		T popNow(bool *empty, T fallbackValue) {
+			std::unique_lock<std::mutex> lock(this->d_mutex);
+			*empty = this->d_queue.empty();
+			if (*empty) {
+				return fallbackValue;
+			}
+			T rc(std::move(this->d_queue.back()));
+			this->d_queue.pop_back();
+			return rc;
+		}
+	};
+
+}
+
+
+//Matrix sizes
+int MX	= 1000;
+
+//all the matrix
+long int **matrix1, **matrix2, **matrix;
+
+void val(){
+	long int i, j;
+	for(i=0; i<MX; i++){
+		for(j=0; j<MX; j++){
+			matrix1[i][j] = 4;
+			matrix2[i][j] = 5;
+			matrix[i][j] = 0;
+		}
+	}	
+}
+void printMatrix(long int **matrix){		
+	int i, j;
+	for(i=0; i<MX; i++){
+		printf("\n");
+		for(j=0; j<MX; j++){
+			printf("%ld ", matrix[i][j]);
+		}
+	}
+	printf("\n");				
+}
+//matrix multiplication algorithm
+void multiply(unsigned int threads){
+	comm::queue<long int> *lines = new comm::queue<long int>;
+	for(long int i=0; i<MX; i++){
+		lines->push(i);
+	}
+	// for(long int i=0; i<threads; i++){
+	// 	lines->push(-1);
+	// }
+	std::vector<std::thread> th;
+	for (int t = 0; t<threads; t++){
+		th.push_back(std::thread ([lines]{
+			bool empty = 0;
+			for(long int i = lines->popNow(&empty,-1); !empty; i = lines->popNow(&empty,-1)){
+				for(long int j=0; j<MX; j++){
+					for(long int k=0; k<MX; k++){
+						matrix[i][j] += (matrix1[i][k] * matrix2[k][j]);
+					}
+				}
+			}
+		}));
+	}
+	for (int t = 0; t<threads; t++){
+		th[t].join();
+	}
+}
+//main function
+int main(int argc, char const *argv[]){
+
+    char *MX_env = getenv("MX");
+    if (MX_env != NULL) {
+        MX = atoi(MX_env);
+    }
+	unsigned int threads = std::thread::hardware_concurrency();
+	if (argc >1 && argv != NULL){
+		threads = atoi(argv[1]);
+	}
+
+	matrix = new long int*[MX];  
+	matrix1 = new long int*[MX];
+	matrix2 = new long int*[MX];
+
+	for (long int i=0; i < MX; i++){
+	    matrix[i] = new long int[MX];
+	    matrix1[i] = new long int[MX];
+	    matrix2[i] = new long int[MX];
+	}
+	
+	//assigning fixed values to the matrix			
+	val();
+
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	//matrix multiplication algorithm call
+	multiply(threads);
+	
+	std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+  	auto elapsed_secs = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+
+	std::cout << "threads: " << threads << "\tSIZE: " << MX << "\ttime: "<< elapsed_secs/1000000.0 << std::endl;
+	//printing the resultant matrix (you may comment when bigger sizes will be set-up)
+	//printMatrix(matrix);
+
+	delete matrix;
+	delete matrix1;
+	delete matrix2;
+}
